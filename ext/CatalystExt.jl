43b6ce0da4db9375306a29cbea1f2c3a5a13c29d
jo-ap <johannes.apelt@posteo.de>
2025-03-18 09:30:56 +0100

    added extension for Catalyst.jl

---
+++ ext/CatalystExt.jl
@@ -0,0 +1,27 @@
+module CatalystExt
+
+using Catalyst
+using DocStringExtensions
+using TikhonovFenichelReductions 
+using TikhonovFenichelReductions.Oscar
+
+"""
+    $(TYPEDSIGNATURES)
+
+Constructor for `ReductionProblem` Type from a `ReactionSystem` defined with
+`Catalyst.jl`.
+"""
+function TikhonovFenichelReductions.ReductionProblem(
+  reaction_network::ReactionSystem,
+  s::Int;
+  idx_slow_fast::Vector{Bool}=[true for _ in parameters(reaction_network)])
+  # generate input for ReductionProblem
+  ode_system = convert(ODESystem, reaction_network)
+  _f = Meta.eval(generate_function(ode_system)[1]);
+  f(x,p) = Base.invokelatest(_f, x, p, 0)
+  x = [replace(str, "(t)" => "") for str in string.(species(reaction_network))]
+  p = string.(parameters(reaction_network))
+  return ReductionProblem(f, x, p, s; idx_slow_fast=idx_slow_fast)
+end
+
+end
